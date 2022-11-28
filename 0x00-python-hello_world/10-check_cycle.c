0f369ea1284be5e246dc2d98769e2cfb6cc99e77
Gatwan <abigailgathigia@gmail.com>
2022-11-28 14:40:08 +0000

    Error

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+*check_cycle - function that checks if a singly linked list has a cycle in it
+*@list: pointer to list
+*Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+
+}
