eaa28f12cb4eb7b66ba9e3533a35f9c988fa23ab
Gatwan <abigailgathigia@gmail.com>
2022-11-28 12:43:27 +0000

    print letters

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = print(word[:3])
+word_last_2 = print(word[8:9])
+middle_word = print(word[1:8])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
