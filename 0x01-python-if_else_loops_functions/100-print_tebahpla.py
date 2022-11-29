922b0cabb7f81cc168d00710b50828e8be67ea7d
Gatwan <abigailgathigia@gmail.com>
2022-11-29 14:38:22 +0000

    prints the ASCII alphabet in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for letter in reversed(range(97, 123)):
-    print("{}".format(chr(letter) if letter % 2 == 0 else chr(letter - 32)), end='')
+for alPH in reversed(range(97, 123)):
+    print("{}".format(chr(alPH) if alPH % 2 == 0 else chr(alPH - 32)), end='')
