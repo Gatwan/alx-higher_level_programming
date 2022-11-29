c69fff2b79e8bab9d77c3ebadd2780e3bd22501c
Gatwan <abigailgathigia@gmail.com>
2022-11-29 09:25:48 +0000

    Fix new line error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,8 +10,8 @@ else:
 print("Last digit of {} is {}".format(number, lastdigit),end='')
 
 if lastdigit > 5:
-    print(" and is greater than 5")
+    print(" and is greater than 5\n")
 elif lastdigit == 0:
-    print(" and is 0")
+    print(" and is 0\n")
 else:
-    print(" and is less than 6 and not 0")
+    print(" and is less than 6 and not 0\n")
