d9a019061901af2b78a83922d5cc36a5e8d9b7fc
Gatwan <abigailgathigia@gmail.com>
2022-11-29 12:24:33 +0000

    Fix style error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 10):
     for num1 in range(num + 1, 10):
-        if num == 8  and num1 == 9:
-            print("89")
-        elif num != num1:
-            print("{:d}{:d}".format(num, num1), end=', ')
+    if num == 8  and num1 == 9:
+        print("89")
+    elif num != num1:
+        print("{:d}{:d}".format(num, num1), end=', ')
