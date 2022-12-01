3d78004ba1dfcf1493258ffb32d3e2d53271ecd2
Gatwan <abigailgathigia@gmail.com>
2022-12-01 13:22:12 +0000

    prints all the names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     hidden_discovery = dir(hidden_4)
-    for name in range(len(hidden_discovery)):
-        if hidden_discovery[name][0] != '_' and hidden_discovery[name][1] != '-':
-            print(hidden_discovery[name])
+    for n in range(len(hidden_discovery)):
+        if hidden_discovery[n][0] != '_' and hidden_discovery[n][1] != '-':
+            print(hidden_discovery[n])
