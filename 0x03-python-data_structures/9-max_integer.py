3a8718d24f0ba7c3958119c7a76d81fee9555afd
Gatwan <abigailgathigia@gmail.com>
2022-12-05 13:33:55 +0000

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        maxnum = my_list[0]
+
+        for a in my_list:
+            if a > maxnum:
+                maxnum = a
+    return (maxnum)
