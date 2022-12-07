48557976cfad6058c7a44e9d52e4dd9cc307b0ac
Gatwan <abigailgathigia@gmail.com>
2022-12-07 10:28:48 +0000

    square value of all integers of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    sq_matrix=[]
+    for r in matrix:
+        sq_r=[]
+    for num in r:
+        sq_num = num * num
+    sq_r.append(sq_num)
+    sq_matrix.append(sq_r)
+    return sq_matrix
