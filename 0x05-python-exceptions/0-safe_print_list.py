ec66b94f46632cf5f20256420f5d3cb1a2cc7371
Gatwan <abigailgathigia@gmail.com>
2022-12-19 11:19:00 +0000

    Prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    if my_list:
+        length = 0
+
+        try:
+            for i in range(0, x):
+                print("{}".format(my_list[i]), end='')
+                length += 1
+            print()
+            return length
+        except:
+            print()
+            return length
+    else:
+        print()
+        return 0
