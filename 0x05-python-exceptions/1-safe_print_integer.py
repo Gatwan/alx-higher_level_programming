048d2f2c05fb6f1cd7cac8201845031ba3dc1837
Gatwan <abigailgathigia@gmail.com>
2022-12-19 12:10:41 +0000

    Fix: Code error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(value), end="")
+        print()
         return True
     except ValueError:
         return False
