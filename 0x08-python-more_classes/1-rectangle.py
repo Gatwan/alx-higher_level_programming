2efd20760c1d62efa7040e532b6ab7f7d53f7198
Gatwan <abigailgathigia@gmail.com>
2023-01-03 07:47:55 +0000

    Add File: Defines a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" This class represents a rectangle """
+
+class Rectangle:
+    """ Class that defines a Rectangle """
+    def __init__(self, width=0, height=0):
+        """ Instantiation with optional width and height """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Private instance attribute """
+        return self._Rectangle__width
+
+    @width.setter
+    def width(self, value):
+        """ Property setter for width """
+        if not isinstance(value, int):
+            """ Check if width value is an integer """
+            raise TypeError("width must be an integer")
+        if value < 0:
+            """ Check if width value is greater than 0 """
+            raise ValueError("width must be >= 0")
+        self._Rectangle__width = value
+
+    @property
+    def height(self):
+        """ Private instance attribute """
+        return self._Rectangle__height
+
+    @height.setter
+    def height(self, value):
+        """ Property setter for height """
+        if not isinstance(value, int):
+            """ Check if height value is an integer """
+            raise TypeError("height must be an integer")
+        if value < 0:
+            """ Check if height value is greater than or equal to zero """
+            raise ValueError("height must be >= 0")
+        self._Rectangle__height = value
