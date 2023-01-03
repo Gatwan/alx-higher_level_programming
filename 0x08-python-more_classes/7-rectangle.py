10fbdc8c214011dc9dc4b0b29100483b74dd40c0
Gatwan <abigailgathigia@gmail.com>
2023-01-03 12:31:13 +0000

    Fix: Pycodestyle

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
 
     number_of_instances = 0
     print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         """ Instantiation with optional width and height """
         self.width = width
