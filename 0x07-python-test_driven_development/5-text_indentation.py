d61c4af33b0dcf8113fd0e81437e2755c1c48638
Gatwan <abigailgathigia@gmail.com>
2023-01-04 15:29:11 +0000

    Add file: Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" function that prints a text with 2 new lines
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """ text must be a string """
+    if not isinstance(text, str):
+        raise TypeError("ext must be a string")
+
+    i = 0
+    while i < len(text):
+        if text[i] == " ":
+            i += 1
+            continue
+        else:
+            break
+
+    while i < len(text) and text[i]:
+        if text[i] in ".:?":
+            print(text[i])
+            print()
+            i += 1
+            if i < len(text):
+                while i < len(text):
+                    if text[i] == " ":
+                        i += 1
+                        continue
+                    else:
+                        break
+        if text[i] == "\n":
+            print(text[i], end="")
+            i += 1
+            if i < len(text):
+                while i < len(text):
+                    if text[i] == " ":
+                        i += 1
+                        continue
+                    else:
+                        break
+        else:
+            print(text[i], end="")
+            i += 1
