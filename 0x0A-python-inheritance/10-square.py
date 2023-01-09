3a13abb845d4783233a19b42da44268afe43b328
Gatwan <abigailgathigia@gmail.com>
2023-01-09 17:34:58 +0000

    Fix code error

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" Defines a square that inherits from a Rectangle """
+"""Defines square rectangle subclass"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Represents a square """
+    """Representing a square."""
 
     def __init__(self, size):
-        """ size must be private 
-            size must be a positive integer """
-    self.integer_validator("size", size)
-    super().__init__(size, size)
-    self.__size = size
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
