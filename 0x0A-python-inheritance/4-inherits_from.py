56d9e7f8caf1e0d1b4144ae76daf838d7352446c
Gatwan <abigailgathigia@gmail.com>
2023-01-09 16:33:26 +0000

    Fix pycodestyle error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """ returns True if the object is an instance of a
-        class that inherited (directly or indirectly) 
+        class that inherited (directly or indirectly)
         from the specified class; otherwise False """
     if type(obj) is not a_class and issubclass(type(obj), a_class):
         return True
