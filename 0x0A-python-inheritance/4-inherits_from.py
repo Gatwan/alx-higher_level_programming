8c10c1709fecb26b7c287a7a07c1f2bf55761b89
Gatwan <abigailgathigia@gmail.com>
2023-01-09 16:31:56 +0000

    Add file 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Inherits class """
+
+
+def inherits_from(obj, a_class):
+    """ returns True if the object is an instance of a
+        class that inherited (directly or indirectly) 
+        from the specified class; otherwise False """
+    if type(obj) is not a_class and issubclass(type(obj), a_class):
+        return True
+    return False
