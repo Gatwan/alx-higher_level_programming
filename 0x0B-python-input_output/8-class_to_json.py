ab48922f873ead04e068663a8b4a955b0cba5439
Gatwan <abigailgathigia@gmail.com>
2023-01-10 14:32:39 +0000

    Add file 5-save_to_json_string.py

--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" JSON serialization of an object """
-import json
-
-def class_to_json(obj):
-    """ returns dictionary description with simple data structure """
-    return obj.__dict__
