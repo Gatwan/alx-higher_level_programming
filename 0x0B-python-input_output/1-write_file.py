847c4ad458524214c8bb5e23846c1eab2bce6870
Gatwan <abigailgathigia@gmail.com>
2023-01-10 09:13:51 +0000

    Add file 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,6 @@
 
 def write_file(filename="", text=""):
     """ returns the number of characters written """
-    count = 0
-    with open('tests/my_first_file.txt', 'w', encoding="utf-8") as f:
-        count = f.write(text)
-    return count
+    
+    with open('tests/my_first_file.txt', 'w+', encoding="utf-8") as f:
+        return f.write(text)
