e99268ecfb4f6c4d88a34d05e367411275d331d2
Gatwan <abigailgathigia@gmail.com>
2023-01-10 10:09:50 +0000

    Fix pycodestyle error

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """ appends a string at the end of a text file 
+    """ appends a string at the end of a text file
         returns the number of characters added """
 
     with open(filename, 'a+', encoding="utf-8") as f:
