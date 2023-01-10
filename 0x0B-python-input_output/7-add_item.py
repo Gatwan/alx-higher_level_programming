5546c27b047a16e4e4342c90e0aba922b0ccf5de
Gatwan <abigailgathigia@gmail.com>
2023-01-10 13:52:54 +0000

    Fix code error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """ Adds all arguments to a python list """
 import sys
+import os.path
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__\
-("6-load_from_json_file").load_from_json_file
+save_file = __import__("5-save_to_json_file").save_to_json_file
+load_file = __import__("6-load_from_json_file").load_from_json_file
 
-    try:
-        data = load_from_json_file("add_items.json")
-    except Exception:
-        data = []
+my_list = []
+if os.path.exists("add_item.json"):
+    my_list = load_file("add_item.json")
 
-    for item in sys.argv[1:]:
-        data.append(item)
+for arg in sys.argv[1:]:
+    my_list.append(arg)
 
-    save_to_json_file(data, "add_item.json")
+save_file(my_list, "add_item.json")
