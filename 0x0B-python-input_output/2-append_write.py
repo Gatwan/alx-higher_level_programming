d9e275a8577dc91613edafab18a72f311e38af83
Gatwan <abigailgathigia@gmail.com>
2023-01-10 10:08:04 +0000

    Fix code error

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,6 @@
 def append_write(filename="", text=""):
     """ appends a string at the end of a text file 
         returns the number of characters added """
-    new_line = 0
-    with open('tests/file_append.txt', 'a', encoding="utf-8") as f:
-        new_line = f.write(text)
-    return new_line
+
+    with open(filename, 'a+', encoding="utf-8") as f:
+        return f.write(text)
