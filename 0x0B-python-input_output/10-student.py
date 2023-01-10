5db594d186b5b308d554156d822194ee21dc9f82
Gatwan <abigailgathigia@gmail.com>
2023-01-10 18:06:03 +0000

    Add file 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Defines a class """
+
+
+class Student:
+    """ Student class """
+
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        """ public instance attributes"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation """
+        if isinstance(attrs, list) and\
+            all(isinstance(item, str) for item in attrs):
+            result ={}
+            for i in attrs:
+                try:
+                    result[i] = self.__dict__[i]
+                except Exception:
+                    pass
+            return result
+        return self.__dict__
