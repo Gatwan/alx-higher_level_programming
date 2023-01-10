56eded10f6ac859717ff0d36868dbbf6fdf126de
Gatwan <abigailgathigia@gmail.com>
2023-01-10 13:24:56 +0000

    Fix code error

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,8 +4,4 @@ import json
 
 def class_to_json(obj):
     """ returns dictionary description with simple data structure """
-    result = {}
-    for key, value in obj.__dict__.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            result[key] = value
-        return result
+    return obj.__dict__
