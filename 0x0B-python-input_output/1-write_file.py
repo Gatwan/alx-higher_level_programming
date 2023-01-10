fb1334ff6af1eea9914be3e69cfa93e3d35645c1
Gatwan <abigailgathigia@gmail.com>
2023-01-10 10:04:41 +0000

    Fix pycodestyle error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """ returns the number of characters written """
-    
+
     with open(filename, 'w+', encoding="utf-8") as f:
         return f.write(text)
