5475e9e5a86b8af70aac49f3feac18f2204d9a88
Gatwan <abigailgathigia@gmail.com>
2023-01-10 08:18:47 +0000

    Add file 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ reads a text file """
-    with open('my_file_0.txt', 'r', encoding="utf-8") as f:
-        read_file = f.read()
-    f.closed
+    with open('tests/my_file_0.txt', encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
