fe0beec738d6653e8545958e4d5a48d9a2d1e5a2
Gatwan <abigailgathigia@gmail.com>
2023-01-10 19:23:58 +0000

    Fix code error

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-""" Defines a class """
+""" Module that defines the class Student
+"""
 
 
 class Student:
-    """ Student class """
-
-    first_name = ""
-    last_name = ""
-    age = 0
+    """ Class to create student instances """
 
     def __init__(self, first_name, last_name, age):
-        """ public instance attributes"""
-
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Retrieves a dictionary representation """
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
 
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
 
-        if isinstance(attrs, list) and \
-        all(isinstance(item, str) for item in attrs):
-            result ={}
+            d_list = {}
 
-            for i in attrs:
-                try:
-                    result[i] = self.__dict__[i]
-                except Exception:
-                    pass
-            return result
-        return self.__dict__
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
