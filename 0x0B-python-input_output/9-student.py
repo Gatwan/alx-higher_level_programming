8e0c4a2e3e7500e381eda1029b7294778a7391c0
Gatwan <abigailgathigia@gmail.com>
2023-01-10 14:50:31 +0000

    Add file 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-""" Defines a class Student """
+"""Defines student class"""
 
 
 class Student:
-    """ defines a student """
+    """Student object"""
 
     first_name = ""
     last_name = ""
     age = 0
 
     def __init__(self, first_name, last_name, age):
-        """ public instance """
+        """Initializing instance"""
 
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-        """ retrieves a dictionary representation of Student instance """
+        """"""
         return self.__dict__
