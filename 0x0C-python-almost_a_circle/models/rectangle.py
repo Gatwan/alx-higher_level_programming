244983b1a347804feefd42842b6af51aee0a4cba
Gatwan <abigailgathigia@gmail.com>
2023-01-16 15:37:34 +0000

    Add setter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,17 +20,37 @@ class Rectangle(Base):
         """ width getter """
         return self.__width
 
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        self.__width = value
+
     @property
     def height(self):
         """ height getter """
         return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        self.__height = value
 
     @property
     def x(self):
         """ x getter """
         return self.__x
 
+    @x.setter
+    def x(self, value):
+        """ x setter """
+        self.__x = value
+
     @property
     def y(self):
         """ y getter """
         return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ y setter """
+        self.__y = value
