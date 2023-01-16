fa25d246f35c60d7cd95d3852d414c659e177fea
Gatwan <abigailgathigia@gmail.com>
2023-01-16 18:44:34 +0000

    Fix code error

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ Initializes instances """
-        super().__init__(width, height, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ str special method """
@@ -20,3 +20,17 @@ class Square(Rectangle):
         str_wh = "{}/{}".format(self.width, self.height)
 
         return str_square + str_id + str_xy + str_wh
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
