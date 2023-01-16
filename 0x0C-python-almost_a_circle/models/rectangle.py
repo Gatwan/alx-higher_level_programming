67455c5669a8bbdf0b9711e0317cef61e9eee757
Gatwan <abigailgathigia@gmail.com>
2023-01-16 15:57:58 +0000

    Addition of attributes validator

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,6 +23,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ width setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -33,6 +37,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ height setter """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -43,6 +51,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ x setter """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -53,4 +65,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ y setter """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
