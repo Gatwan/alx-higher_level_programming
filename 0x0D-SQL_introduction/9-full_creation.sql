228c365859abf5776766f01f5963b556218a7d7a
Gatwan <abigailgathigia@gmail.com>
2023-02-14 18:53:56 +0300

    [Add file]: 9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- Script that creates a second table
+CREATE INTO IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT );
+INSERT INTO second_table(id, name, score)
+	VALUES(1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
