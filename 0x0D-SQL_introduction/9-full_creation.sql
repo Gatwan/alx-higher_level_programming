bc3e9c34f113588e826137e2d4ea4a06a91e220e
Gatwan <abigailgathigia@gmail.com>
2023-02-14 18:57:57 +0300

    [Add file]: 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,9 +2,11 @@
 CREATE INTO IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
-	score INT );
+	score INT
+);
 INSERT INTO second_table(
 	id, 
 	name, 
-	score)
+	score
+)
 VALUES(1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
