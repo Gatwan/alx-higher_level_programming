3bf5ae32ac12657ca2bd0d0c5aeb41af760d98f7
Gatwan <abigailgathigia@gmail.com>
2023-02-15 13:05:50 +0300

    [Add file]: 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- creates the MySQL server user user_0d_1
-IF NOT EXISTS CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
