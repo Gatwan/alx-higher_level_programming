e589f60f59739d03519c6d6e9560e4b8e6557e08
Gatwan <abigailgathigia@gmail.com>
2023-02-15 14:05:50 +0300

    [Add file]: 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- script that creates the database hbtn_0d_usa and the table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT UNIQUE NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
