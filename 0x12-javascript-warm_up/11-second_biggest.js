375173a005b04926704c84bda0772d14a1762703
Gatwan <abigailgathigia@gmail.com>
2023-03-13 22:48:54 +0300

    Fix error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const argv = process.argv;
 argv.splice(0, 2);
 if (argv.length === 0) {
-  console.log(0);
+  console.log('0');
 } else {
   argv.sort((a, b) => a - b);
   console.log(+argv[argv.length - 2]);
