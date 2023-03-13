50ceef65fd151205489e42a33fb81fa8def7a4b2
Gatwan <abigailgathigia@gmail.com>
2023-03-13 23:04:57 +0300

    Fix code error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,20 @@
 #!/usr/bin/node
-const argv = process.argv;
-argv.splice(0, 2);
-if (argv.length === 0) {
-  console.log('0');
-} else {
-  argv.sort((a, b) => a - b);
-  console.log(+argv[argv.length - 2]);
+
+let biggest = 0;
+let i;
+const arrayNumbers = [];
+
+for (i = 2; i < process.argv.length; i++) {
+  if (Number.isNaN(parseInt(process.argv[i])) === false) {
+    arrayNumbers[i - 2] = parseInt(process.argv[i]);
+  }
 }
+
+if (arrayNumbers.length > 1) {
+  biggest = Math.max.apply(null, arrayNumbers);
+  i = arrayNumbers.indexOf(biggest);
+  arrayNumbers[i] = -Infinity;
+  biggest = Math.max.apply(null, arrayNumbers);
+}
+
+console.log(biggest);
