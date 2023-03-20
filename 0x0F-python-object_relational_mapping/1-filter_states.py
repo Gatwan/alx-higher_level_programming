fcda3e138dd1cd80536f6b0e03d0e872558ada00
Gatwan <abigailgathigia@gmail.com>
2023-03-20 14:13:05 +0300

    Add file 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module to list database items"""
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """List database items"""
+    database = MySQLdb.connect(host='localhost', port=3306,
+                                user=argv[1], passwd=argv[2], db=argv[3])
+    curr = database.cursor()
+    query = 'SELECT * FROM states WHERE LIKE BINARY"N%" ORDER BY id ASC'
+    curr.execute(query)
+    data = curr.fetchall()
+    for state in data:
+        print(state)
+    curr.close()
+    database.close()
+
+    if __name__ == '__main__':
+        main()
