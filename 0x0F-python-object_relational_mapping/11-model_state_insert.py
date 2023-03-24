d272affb9133b8ea3e2464a46586f917002a5bd3
Gatwan <abigailgathigia@gmail.com>
2023-03-24 23:16:40 +0300

    Fix code error

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-""" Script that lists all State objects from the database hbtn_0e_6_usa
 """
+Adds the State object "Lousiana" to the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from sys import argv
-
-
-def main():
-    username, password, database = argv[1:]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    new_state = State()
-    new_state.name = "Louisiana"
-    session.add(new_state)
-    state = session.query(State).filter_by(name="Louisiana").first()
-    print(state.id)
-    session.commit()
 
 
 if __name__ == '__main__':
-    main()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    newState = State(name='Louisiana')
+    session.add(newState)
+    session.commit()
+
+    print(newState.id)
