eb700941dbc7d828238e46cf710106a69f9e4ae2
Gatwan <abigailgathigia@gmail.com>
2023-04-05 14:16:10 +0300

    Fix code error

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ Displays response header value """
-import requests
+from requests import get
 from sys import argv
 
 
 if __name__ == "__main__":
     """ sends request to URL and displays header """
-    try:
-        r = requests.get(argv[1])
-        print(r.headers['X-Request-Id'])
-    except:
-        pass
+    r = get(argv[1])
+    req_id = r.headers.get('X-Request-Id')
+    print(req_id)
