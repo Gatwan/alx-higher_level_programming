b02a5a4b99c4ae4db12c8f27951babb58c64acf1
Gatwan <abigailgathigia@gmail.com>
2023-04-05 14:11:28 +0300

    Fix code error

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,5 +6,8 @@ from sys import argv
 
 if __name__ == "__main__":
     """ sends request to URL and displays header """
-    r = requests.get(argv[1])
-    print(r.headers['X-Request-Id'])
+    try:
+        r = requests.get(argv[1])
+        print(r.headers['X-Request-Id'])
+    except:
+        pass
